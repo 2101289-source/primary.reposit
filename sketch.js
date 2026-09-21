1bfead09a6698e6c19c6f0e78c20157522105c0a
Sophia <2101289@spsd.sk.ca>
2026-09-21 14:10:08 -0600

    Day 1

---
+++ sketch.js
@@ -0,0 +1,27 @@
+//First assingment
+//Sophia Eruero
+// 09/21/26
+
+
+async function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+//plan- the first world will be a normal world.
+//the second  will be a sky world
+//the third will be a candy wonderland 
+function draw() {
+  background("skyblue");
+
+
+  //test point
+  circle(0,0,500,);
+  fill("lightyellow");
+  stroke("white");
+  //ok that works
+  //this is the sun of the first world 
+
+  triangle(600,600,600,200, 660, 250);
+
+
+}
